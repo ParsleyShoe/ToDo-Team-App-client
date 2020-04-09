2fb5e9acf761bf33f858745842e892287b743cc6
max-student <max-student@maxtrain.com>
2020-04-09 10:56:55 -0400

    To-do list user

--- src/app/todo/todo-list-user/todo-list-user.component.ts
+++ src/app/todo/todo-list-user/todo-list-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo.class';
+import { TodoService } from '../todo.service';
+import { User } from 'app/user/user.class';
+import { SystemService } from 'app/system.service';
 
 @Component({
   selector: 'app-todo-list-user',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListUserComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[] = [];
+  user: User;
+  todo: Todo = new Todo()
+
+  constructor(
+    private todosvc: TodoService,
+    private systemsvc: SystemService,
+  ) { }
 
   ngOnInit(): void {
+    this.todo.assignedUserId = this.systemsvc.user.id;
+    this.todosvc.listByUser(this.todo.assignedUserId).subscribe(
+      res => {
+        this.todos = res;
+        console.debug("To-Do List for User: ", res);
+      },
+      err =>{
+        console.error("Error getting To-Dos ", err);
+     }
+    );
+    
   }
 
 }
